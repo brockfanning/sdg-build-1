6fc770dfe7644a8cc86ada153aab7e635e03855b
Lucy <43404358+LucyGwilliamAdmin@users.noreply.github.com>
2021-09-24 13:20:18 +0100

    Update MetadataReportService.py

--- sdg/MetadataReportService.py
+++ sdg/MetadataReportService.py
@@ -44,7 +44,7 @@ class MetadataReportService(Loggable):
         self.metadata_field_store = None
 
 
-    def get_metadata_field_store(self, metadata):
+    def get_metadata_field_store(self):
         """Analyzes the data in and compiles information about indicators.
         Returns
         -------
