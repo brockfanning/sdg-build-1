705db4e8e197f1cc8cfac8548f2fce548c24b127
Lucy <43404358+LucyGwilliamAdmin@users.noreply.github.com>
2021-09-24 13:12:05 +0100

    Update MetadataReportService.py

--- sdg/MetadataReportService.py
+++ sdg/MetadataReportService.py
@@ -210,6 +210,7 @@ class MetadataReportService(Loggable):
 
     def get_indicators_dataframe(self, info):
         grouped = self.group_metadata_field_store_by_indicator()
+        store = self.get_metadata_field_store()
         print(grouped)
         rows = []
         for indicator in grouped:
@@ -218,7 +219,13 @@ class MetadataReportService(Loggable):
                 continue
             rows.append({
                 'Indicator': self.get_indicator_link(indicator),
-                'computation_units': ', '.join(metadata_field_links),
+                'Computation units': ', '.join(self.get_metadata_field_value_link(store['computation_units']['values'][store['computation_units']['indicators'][indicator]])),
+                'Data non-statistical': ', '.join(self.get_metadata_field_value_link(store['data_non_statistical']['values'][store['data_non_statistical']['indicators'][indicator]])),
+                'Data show map': ', '.join(self.get_metadata_field_value_link(store['data_show_map']['values'][store['data_show_map']['indicators'][indicator]])),
+                'National geographical coverage': ', '.join(self.get_metadata_field_value_link(store['national_geographical_coverage']['values'][store['national_geographical_coverage']['indicators'][indicator]])),
+                'Reporting status': ', '.join(self.get_metadata_field_value_link(store['reporting_status']['values'][store['reporting_status']['indicators'][indicator]])),
+                'UN designated tier': ', '.join(self.get_metadata_field_value_link(store['un_designated_tier']['values'][store['un_designated_tier']['indicators'][indicator]])),
+                'Graph type': ', '.join(self.get_metadata_field_value_link(store['graph_type']['values'][store['graph_type']['indicators'][indicator]]))
             })
                 
         df = pd.DataFrame(rows, columns=['Indicator', 'metadata_fields'])
