d2a077aa167320be0f223bbd3b68c3076c4781e3
Lucy <43404358+LucyGwilliamAdmin@users.noreply.github.com>
2021-09-24 15:25:29 +0100

    Update MetadataReportService.py

--- sdg/MetadataReportService.py
+++ sdg/MetadataReportService.py
@@ -218,13 +218,13 @@ class MetadataReportService(Loggable):
                 continue
             rows.append({
                 'Indicator': self.get_indicator_link(indicator),
-                'Computation units': ', '.join(self.get_metadata_field_value_link(store['computation_units']['values'][store['computation_units']['indicators'][indicator]])),
-                'Data non-statistical': ', '.join(self.get_metadata_field_value_link(store['data_non_statistical']['values'][store['data_non_statistical']['indicators'][indicator]])),
-                'Data show map': ', '.join(self.get_metadata_field_value_link(store['data_show_map']['values'][store['data_show_map']['indicators'][indicator]])),
-                'National geographical coverage': ', '.join(self.get_metadata_field_value_link(store['national_geographical_coverage']['values'][store['national_geographical_coverage']['indicators'][indicator]])),
-                'Reporting status': ', '.join(self.get_metadata_field_value_link(store['reporting_status']['values'][store['reporting_status']['indicators'][indicator]])),
-                'UN designated tier': ', '.join(self.get_metadata_field_value_link(store['un_designated_tier']['values'][store['un_designated_tier']['indicators'][indicator]])),
-                'Graph type': ', '.join(self.get_metadata_field_value_link(store['graph_type']['values'][store['graph_type']['indicators'][indicator]]))
+                'Computation units': ', '.join(self.get_metadata_field_value_link(store['computation_units']['values'][store['computation_units']['indicators'][indicator]])) if indicator in store['computation_units']['indicators'] else ', ',
+                'Data non-statistical': ', '.join(self.get_metadata_field_value_link(store['data_non_statistical']['values'][store['data_non_statistical']['indicators'][indicator]])) if indicator in store['data_non_statistical']['indicators'] else ', ',
+                'Data show map': ', '.join(self.get_metadata_field_value_link(store['data_show_map']['values'][store['data_show_map']['indicators'][indicator]])) if indicator in store['data_show_map']['indicators'] else ', ',
+                'National geographical coverage': ', '.join(self.get_metadata_field_value_link(store['national_geographical_coverage']['values'][store['national_geographical_coverage']['indicators'][indicator]])) if indicator in store['national_geographical_coverage']['indicators'] else ', ',
+                'Reporting status': ', '.join(self.get_metadata_field_value_link(store['reporting_status']['values'][store['reporting_status']['indicators'][indicator]])) if indicator in store['reporting_status']['indicators'] else ', ',
+                'UN designated tier': ', '.join(self.get_metadata_field_value_link(store['un_designated_tier']['values'][store['un_designated_tier']['indicators'][indicator]])) if indicator in store['un_designated_tier']['indicators'] else ', ',
+                'Graph type': ', '.join(self.get_metadata_field_value_link(store['graph_type']['values'][store['graph_type']['indicators'][indicator]])) if indicator in store['graph_type']['indicators'] else ', '
             })
                 
         df = pd.DataFrame(rows, columns=['Indicator', 'metadata_fields'])
