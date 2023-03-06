e187faaed2bf32e2124f275b4652a8c3d9be600a
Brock Fanning <brockfanning@gmail.com>
2023-03-05 20:41:21 -0500

    Allow prose schema to be datetime

--- sdg/schemas/SchemaInputOpenSdg.py
+++ sdg/schemas/SchemaInputOpenSdg.py
@@ -88,7 +88,7 @@ class SchemaInputOpenSdg(SchemaInputBase):
                 jsonschema_field[jsonschema_key] = prose_field[prose_key]
 
         # Most Prose.io fields are general text, numbers, or true/false.
-        jsonschema_field['type'] = ['string', 'integer', 'number', 'boolean']
+        jsonschema_field['type'] = ['string', 'integer', 'number', 'boolean', 'datetime']
 
         # Everything else depends on what kind of element it is.
         el = prose_field['element']
